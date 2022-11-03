6354179bb6ca21579dba6978c1c538c66f842343
Laura Suzany <83149455+LauraSuzany@users.noreply.github.com>
2022-11-02 22:05:39 -0300

    Feat: Criando contexto

---
+++ Context/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movie.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+           : base(options)
+        {
+            //Caso não exista o banco ou a tabela cria automaticamente 
+            Database.EnsureCreated();
+        }
+    }
+}
